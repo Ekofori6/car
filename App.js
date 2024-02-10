d0b39df5b056381fbedf04b29ecc7e15f0f180ff
ekofori6 <oforiemmanuelkofi1000@gmail.com>
2024-02-10 14:56:49 +0000

    CarProject

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { SafeAreaView, Text, View } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView className="p-8">
+      <View className="flex-row justify-between">
+        <View className="flex-row gap-2">
+          <View>
+            <FontAwesome name="map-marker" size={24} color="black" />
+          </View>
+
+          <View className="gap-">
+            <Text>Location</Text>
+            <Text className="font-bold">Los Angeles Califonia, US</Text>
+          </View>
+        </View>
+        <View>
+          <Ionicons name="notifications" size={24} color="black" />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
